6940fa3fb40280ef5ae8e9a7abe2e7e187559d10
radhika-modi12 <radhika.h.modi11@gmail.com>
2025-06-11 19:20:33 +0530

    update code

--- contactbook/src/app/contactlist/page.js
+++ contactbook/src/app/contactlist/page.js
@@ -12,7 +12,7 @@ export default function contactList() {
     setUsers(response.data.users);
   };
   return (
-    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400" >
       <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
         <tr className="table-row-border">
           <th>Id</th>
